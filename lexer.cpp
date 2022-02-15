1f5555bd5385621de1fca08003e5452c331c4958
--replace-all <--replace-all>
2022-02-15 23:33:38 +0530

    First commit and repo creation

---
+++ lexer.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+typedef long long int ll;
+#define MAX 1000000007
+
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
