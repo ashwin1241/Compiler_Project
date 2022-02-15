471c7bd1b33b032c01ab9a415310901c4999808f
--replace-all <--replace-all>
2022-02-15 23:56:35 +0530

    refinements to the code to accept multiple line inputs and to end upon encountering eoc;

--- lexer.cpp
+++ lexer.cpp
@@ -8,10 +8,9 @@ typedef long long int ll;
 #define MAX 1000000007
 
 using namespace std;
-int main()
+
+vector<string> input()
 {
-    ll i;
-    cout<<"Enter the input code:\n";
     vector<string> asp;
     string s="";
     while(s[s.size()-1]!=';')
@@ -19,9 +18,28 @@ int main()
         cin>>s;
         asp.push_back(s);
     }
-    for(i=0;i<asp.size();i++)
+    return asp;
+}
+int main()
+{
+    ll i,j;
+    cout<<"Enter the input code:\n";
+    vector<vector<string>> ipCode;
+    vector<string> ipLine,end;
+    end.push_back("eoc;");
+    ipLine.push_back(" ");
+    ipLine.push_back(" ");
+    ipCode.push_back(ipLine);
+    while(ipCode[ipCode.size()-1]!=end)
     {
-        cout<<asp[i]<<" ";
+        ipLine=input();
+        ipCode.push_back(ipLine);
+    }
+    for(i=1;i<ipCode.size()-1;i++)
+    {
+        for(j=0;j<ipCode[i].size();j++)
+        cout<<ipCode[i][j]<<" ";
+        cout<<"\n";
     }
     return 0;
 }
